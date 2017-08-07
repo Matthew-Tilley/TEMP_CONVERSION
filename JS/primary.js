d67a5091cee88465cef02e30d8a05b003b7dacc9
Matt Tilley <mtilley255@gmail.com>
2017-08-07 10:22:57 -0500

    Start of function
    Got the first function working properly / console.log for test purposes only.

--- JS/primary.js
+++ JS/primary.js
@@ -1,4 +1,18 @@
-$(document).ready(function()
-{
-	
-});
+
+function convert(degree)
+	{
+		var x;
+		
+		if (degree == "C")
+		{
+			x = document.getElementById("IF02").value;
+			console.log(x);
+		}
+		
+		if (degree == "F")
+		{
+			x = document.getElementById("IF01").value;
+			console.log(x);
+		}
+		
+	}
