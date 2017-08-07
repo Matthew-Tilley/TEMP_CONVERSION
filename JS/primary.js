be712eff21c74cac34c606b668dfc703123eac8e
Matt Tilley <mtilley255@gmail.com>
2017-08-07 12:44:20 -0500

    More work on function
    Not working properly but want to snapshot.

--- JS/primary.js
+++ JS/primary.js
@@ -6,12 +6,17 @@ function convert(degree)
 		if (degree == "C")
 		{
 			x = document.getElementById("IF02").value;
+			x = x * 9/5 + 32;
+			$("#IF02").val(x);
+			
 			console.log(x);
 		}
+		
 		
 		if (degree == "F")
 		{
 			x = document.getElementById("IF01").value;
+			x = (x - 32) * 5/9;
 			console.log(x);
 		}
 		
